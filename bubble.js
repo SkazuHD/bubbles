f93023002a9ae055df605a4e53eae2b2d6e6fb45
SkazuHD <voda0110@gmail.com>
2023-06-01 13:51:33 +0200

    Data imported

--- bubble.js
+++ bubble.js
@@ -1,20 +1,20 @@
-class Ball{
+class Bubble{
 
     ballDia = 15;
     ballX
     ballY
-    constructor(position=[0,0], radius, color=[0,0,0]) {
-        let [r,g,b] = color
-        this.ballColor = `rgb(${r},${g},${b})`
+    constructor(position=[0,0], radius, color=[0,0,0,1]) {
+        let [r,g,b,a] = color
+        this.ballColor = `rgba(${r},${g},${b},${a})`
         this.ballDia = radius
         this.ballX = position[0]
         this.ballY = position[1]
 
     }
-    moveBall(){
+    move(){
 
     }
-    drawBall(){
+    draw(){
         context.fillStyle = this.ballColor;
         context.beginPath()
         context.arc(this.ballX, this.ballY, this.ballDia, 0, Math.PI *2)
@@ -22,7 +22,12 @@ class Ball{
     }
     changeSize(diffrence=0){
         this.ballDia+=diffrence
-        this.drawBall()
+        this.draw()
+    }
+    changeColor(color=[0,0,0,1]){
+        let [r,g,b,a] = color
+        this.ballColor = `rgba(${r},${g},${b},${a})`
+        this.draw()
     }
 
 }
